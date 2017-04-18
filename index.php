0b7c6d5f6f5f1480eccf40cfa4ba929d7c4ab767
baracos <oscar.baracos@gmail.com>
2017-04-18 21:38:02 +0200

    Added Index page

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$results = scandir('/');
-foreach ($results as $result) {
-    if ($result === '.' or $result === '..') continue;
-    if (is_dir('/' . $result)) {?>
-        <a href="<?php echo $result . "/index.html";?>"><?php echo $result;?></a>
-    <?php}
-}
-?>
